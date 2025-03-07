8b92c08fc796e76ef5398db737408f81b84529a7
Henrique F. Simoes <henrique.simoes@lnls.br>
2025-03-07 10:14:53 -0300

    Remove duplicated comments on variables meaning
    The template itself is the source of truth for the meaning of the
    variables, and it properly documents it. Drop these redudant comments
    from the file that will be the base for each deploy, and replicated for
    each model here.

--- iocs/pimegaIOC/iocBoot/iocPimega/st-pitec-d-pimega135.cmd
+++ iocs/pimegaIOC/iocBoot/iocPimega/st-pitec-d-pimega135.cmd
@@ -1,18 +1,11 @@
 #!../../bin/linux-x86_64/pimegaApp
 
-#ABOUT PIMEGA
-# Prefix for all records
 epicsEnvSet("PREFIX", "PIMEGA:PIMEGA135:")
-# The detector model (0:mobipix; 1:pimega45D; 2:pimega135DL; 3:pimega135D; 4:pimega540D;5:pimega450D; 6:pimega450DS)
 epicsEnvSet("DMODEL", "3");
-# The maximim image width; used for row profiles in the NDPluginStats plugin
 epicsEnvSet("XSIZE",  "1536")
-# The maximim image height; used for column profiles in the NDPluginStats plugin
 epicsEnvSet("YSIZE",  "1536")
 
-# The IP address of the Pimega system
 epicsEnvSet("PIMEGA_MODULE01_IP", "10.255.255.2")
-# The IP port for the command socket
 epicsEnvSet("PIMEGA_PORT", "60000")
 
 epicsEnvSet("FRAME_PORT", "6464")
@@ -22,8 +15,6 @@ epicsEnvSet("CALIBRATION_FILE", "/usr/local/etc/adpimega/pimega135d.ini")
 
 < pimega-template.cmd
 
-#ABOUT PLUGINS
-# Number of Elements. Should be at least X*Y
 epicsEnvSet("NELEMENTS", "2359296")
 
 < plugins.cmd
--- iocs/pimegaIOC/iocBoot/iocPimega/st-pitec-d-pimega540.cmd
+++ iocs/pimegaIOC/iocBoot/iocPimega/st-pitec-d-pimega540.cmd
@@ -1,21 +1,14 @@
 #!../../bin/linux-x86_64/pimegaApp
 
-#ABOUT PIMEGA
-# Prefix for all records
 epicsEnvSet("PREFIX", "PIMEGA:PIMEGA135:")
-# The detector model (0:mobipix; 1:pimega45D; 2:pimega135DL; 3:pimega135D; 4:pimega540D;5:pimega450D; 6:pimega450DS)
 epicsEnvSet("DMODEL", "4");
-# The maximim image width; used for row profiles in the NDPluginStats plugin
 epicsEnvSet("XSIZE",  "3072")
-# The maximim image height; used for column profiles in the NDPluginStats plugin
 epicsEnvSet("YSIZE",  "3072")
 
-# The IP address of the Pimega system
 epicsEnvSet("PIMEGA_MODULE01_IP", "10.255.255.2")
 epicsEnvSet("PIMEGA_MODULE02_IP", "10.255.255.6")
 epicsEnvSet("PIMEGA_MODULE03_IP", "10.255.255.10")
 epicsEnvSet("PIMEGA_MODULE04_IP", "10.255.255.14")
-# The IP port for the command socket
 epicsEnvSet("PIMEGA_PORT", "60000")
 
 epicsEnvSet("FRAME_PORT", "6464")
@@ -25,8 +18,6 @@ epicsEnvSet("CALIBRATION_FILE", "/usr/local/etc/adpimega/pimega540d.ini")
 
 < pimega-template.cmd
 
-#ABOUT PLUGINS
-# Number of Elements. Should be at least X*Y
 epicsEnvSet("NELEMENTS", "9437184")
 
 < plugins.cmd
