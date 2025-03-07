1953fe3b2341be6a561f2d9503d76dd69d685875
Henrique F. Simoes <henrique.simoes@lnls.br>
2025-03-07 10:14:53 -0300

    Remove duplicated comments on variables meaning
    The template itself is the source of truth for the meaning of the
    variables, and it properly documents it. Drop these redudant comments
    from the file that will be the base for each deploy, and replicated for
    each model here.

--- iocs/pimegaIOC/iocBoot/iocPimega/st-pitec-d-pimega450.cmd
+++ iocs/pimegaIOC/iocBoot/iocPimega/st-pitec-d-pimega450.cmd
@@ -1,16 +1,10 @@
 #!../../bin/linux-x86_64/pimegaApp
 
-#ABOUT PIMEGA
-# Prefix for all records
 epicsEnvSet("PREFIX", "PIMEGA:PIMEGA135:")
-# The detector model (0:mobipix; 1:pimega45D; 2:pimega135DL; 3:pimega135D; 4:pimega540D;5:pimega450D; 6:pimega450DS)
 epicsEnvSet("DMODEL", "5");
-# The maximim image width; used for row profiles in the NDPluginStats plugin
 epicsEnvSet("XSIZE",  "15360")
-# The maximim image height; used for column profiles in the NDPluginStats plugin
 epicsEnvSet("YSIZE",  "512")
 
-# The IP address of the Pimega system
 epicsEnvSet("PIMEGA_MODULE01_IP", "10.255.255.2")
 epicsEnvSet("PIMEGA_MODULE02_IP", "10.255.255.3")
 epicsEnvSet("PIMEGA_MODULE03_IP", "10.255.255.4")
@@ -21,7 +15,6 @@ epicsEnvSet("PIMEGA_MODULE07_IP", "10.255.255.8")
 epicsEnvSet("PIMEGA_MODULE08_IP", "10.255.255.9")
 epicsEnvSet("PIMEGA_MODULE09_IP", "10.255.255.10")
 epicsEnvSet("PIMEGA_MODULE10_IP", "10.255.255.11")
-# The IP port for the command socket
 epicsEnvSet("PIMEGA_PORT", "60000")
 
 epicsEnvSet("FRAME_PORT", "6464")
@@ -31,8 +24,6 @@ epicsEnvSet("CALIBRATION_FILE", "/usr/local/etc/adpimega/pimega450d.ini")
 
 < pimega-template.cmd
 
-#ABOUT PLUGINS
-# Number of Elements. Should be at least X*Y
 epicsEnvSet("NELEMENTS", "7864320")
 
 < plugins.cmd
