dd652ea1634ca877fe81eb9e273979cfc05bd746
Henrique F. Simoes <henrique.simoes@lnls.br>
2025-03-18 16:22:07 -0300

    Group start-up scripts variables in logical blocks
    To make it easier for a human to parse the definitions, group them
    visually into four blocks:
    
    - basic settings
    - IP addresses
    - connection ports
    - miscellaneous variables
    
    These blocks seem good by now, but they surely can be redefined as new
    variables are introduced or existing ones removed.

--- iocs/pimegaIOC/iocBoot/iocPimega/st-pimega540d.cmd
+++ iocs/pimegaIOC/iocBoot/iocPimega/st-pimega540d.cmd
@@ -9,8 +9,8 @@ epicsEnvSet("PIMEGA_MODULE01_IP", "10.255.255.2")
 epicsEnvSet("PIMEGA_MODULE02_IP", "10.255.255.6")
 epicsEnvSet("PIMEGA_MODULE03_IP", "10.255.255.10")
 epicsEnvSet("PIMEGA_MODULE04_IP", "10.255.255.14")
-epicsEnvSet("PIMEGA_PORT", "60000")
 
+epicsEnvSet("PIMEGA_PORT", "60000")
 epicsEnvSet("FRAME_PORT", "6464")
 epicsEnvSet("COMMAND_PORT", "5412")
 
